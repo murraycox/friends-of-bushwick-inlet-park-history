cc70a33dc63d589651eef4eee1625f29a9d230b9
Murray Cox <code@murraycox.com>
2024-05-11 22:47:09 -0400

    Initial support for state and history

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 
+    import { goto, pushState } from '$app/navigation';
+	import { page } from '$app/stores';
+
     import NavEras from "$lib/components/NavEras.svelte";
     import NavTimeline from "$lib/components/NavTimeline.svelte";
     import Maps from "$lib/components/Maps.svelte"
@@ -35,10 +38,26 @@
     function onEraClick(event) {
         console.log(`routes/map:onEraClick(event.detail:${JSON.stringify(event.detail)})`); 
         maps.onEraClick(event);
+        // pushState(`/#/era/${event.detail.id}`, {
+		// 	view: 'era',
+        //     id: event.detail.id
+		// });
+        goto(`#/era/${event.detail.id}`, { state: {view: 'era', id: event.detail.id}, noScroll: true });
     }
+
+    // Handle forward/back buttons
+    function popState(event) {
+        // If a state has been provided, we have a "simulated" page
+        // and we update the current page.
+        if (event.state) {
+            // Simulate the loading of the previous page
+            console.log(`routes/:popstate:event.state:${JSON.stringify(event.state)}`);
+            console.log(`routes/:popstate:page.state:${JSON.stringify($page.state)}`);//make sure to use the reactive $, i.e. $page
+        }
+    };
     
 </script>
-
+<svelte:window on:popstate={popState}/>
 <NavEras on:eraClick={onEraClick}/>
 <Maps bind:this={maps} on:featureMouseOver={onMapFeatureMouseOver} on:featureMouseOut={onMapFeatureMouseOut} on:featureClick={onFeatureClick}/>    
 <NavTimeline bind:this={navTimeline} on:chapterMouseOver={onChapterMouseOver} on:chapterMouseLeave={onChapterMouseLeave} on:eraClick={onEraClick}/>
