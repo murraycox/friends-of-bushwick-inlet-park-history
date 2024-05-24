0020ad8c809084591fc7fb1521fa4ff3b0ec3b28
Murray Cox <code@murraycox.com>
2024-05-24 00:12:24 -0400

    Renovated the nav bar, new style, colours and icons

--- src/lib/components/NavEras.svelte
+++ src/lib/components/NavEras.svelte
@@ -30,50 +30,45 @@
             Intro
         </div>
     </a>
-    <div class="story-navigation-button-group">
-        <div class="story-navigation-button-group-header">
-            eras
-        </div>
-        <a href="#" on:click={() => onEraClick("pre-1600s")}>
-            <div class="story-navigation-button-and-label era-pre-colonial"  class:active={$page.url.pathname.includes("/eras/pre-colonial")}>
-                <div class="story-navigation-button">
-                </div>
-                <div class="story-navigation-label">
-                    <span>Pre Colonial</span>
-                </div>
-            </div>
-        </a>
-        <a href="#" on:click={() => onEraClick("early-european-settlement")}>
-            <div class="story-navigation-button-and-label era-early-european-settlement">
-                <div class="story-navigation-button">
-                </div>
-                <div class="story-navigation-label">
-                    <span>Early European Settlement</span>
-                </div>
-            </div>
-        </a>
-        <a href="#" on:click={() => onEraClick("urban-industrial-era")}>
-            <div class="story-navigation-button-and-label era-urban-industrial" class:active={$page.url.pathname.includes("/eras/urban-industrial")}>
-                <div class="story-navigation-button">
-                </div>
-                <div class="story-navigation-label">
-                    <span>Urban + Industrial Era</span>
-                </div>
-            </div>
-        </a>
-        <div class="story-navigation-button-and-label era-migration">
+    <a href="#" on:click={() => onEraClick("pre-1600s")}>
+        <div class="story-navigation-button-and-label era-pre-colonial"  class:active={$page.url.pathname.includes("/eras/pre-colonial")}>
             <div class="story-navigation-button">
             </div>
             <div class="story-navigation-label">
-                <span>Migration</span>
+                <span>pre 1600s</span>
             </div>
         </div>
-        <div class="story-navigation-button-and-label era-activism-deindustrialization">
+    </a>
+    <a href="#" on:click={() => onEraClick("early-european-settlement")}>
+        <div class="story-navigation-button-and-label era-early-european-settlement">
             <div class="story-navigation-button">
             </div>
             <div class="story-navigation-label">
-                <span>Activism + Deindustrialization</span>
+                <span>1620-1855</span>
             </div>
+        </div>
+    </a>
+    <a href="#" on:click={() => onEraClick("urban-industrial-era")}>
+        <div class="story-navigation-button-and-label era-urban-industrial" class:active={$page.url.pathname.includes("/eras/urban-industrial")}>
+            <div class="story-navigation-button">
+            </div>
+            <div class="story-navigation-label">
+                <span>1855-1950</span>
+            </div>
+        </div>
+    </a>
+    <div class="story-navigation-button-and-label era-migration">
+        <div class="story-navigation-button">
+        </div>
+        <div class="story-navigation-label">
+            <span>1950-2005</span>
+        </div>
+    </div>
+    <div class="story-navigation-button-and-label era-activism-deindustrialization">
+        <div class="story-navigation-button">
+        </div>
+        <div class="story-navigation-label">
+            <span>2005-Future</span>
         </div>
     </div>
 </div>  
@@ -89,51 +84,74 @@
         flex-direction: row;
     }
 
-    .story-navigation-button-and-label .story-navigation-button {
-        background-color: lightgray;
-    }
-    .story-navigation-button-and-label .story-navigation-label{
-        color: gray;
-    }
-
-    /* Styles for .era-urban-industrial */
-    .story-navigation-button-and-label.era-urban-industrial:hover .story-navigation-button, .story-navigation-button-and-label.era-urban-industrial.active .story-navigation-button, body.era-urban-industrial .story-narrative-footer {
-        background-color: #A060C1;
-    }
-    .story-navigation-button-and-label.era-urban-industrial:hover .story-navigation-label, .story-navigation-button-and-label.era-urban-industrial.active .story-navigation-label, body.era-urban-industrial #story-narrative h1{
-        color: #A060C1;;
-    }
-
-    /* Styles for .era-early-european-settlement */
-    .story-navigation-button-and-label.era-early-european-settlement:hover .story-navigation-button {
-        background-color: #83BC6E;
-    }
-    .story-navigation-button-and-label.era-early-european-settlement:hover .story-navigation-label{
-        color: #83BC6E;
+    .story-navigation-button {
+        background-color: white;
+        border: 1px solid lightgrey;
     }
 
     /* Styles for .era-pre-colonial */
-    .story-navigation-button-and-label.era-pre-colonial:hover .story-navigation-button, .story-navigation-button-and-label.era-pre-colonial.active .story-navigation-button, body.era-pre-colonial .story-narrative-footer {
-        background-color: #599FF0;
+    .story-navigation-button-and-label.era-pre-colonial .story-navigation-button {
+        background-image: url("/images/icons/era1_icon_5199C7.svg");
+        border-color: #5199C7;
     }
-    .story-navigation-button-and-label.era-pre-colonial:hover .story-navigation-label, .story-navigation-button-and-label.era-pre-colonial.active .story-navigation-label, body.era-pre-colonial #story-narrative h1{
-        color: #599FF0;
+    .story-navigation-button-and-label.era-pre-colonial:hover .story-navigation-button {
+        background-image: url("/images/icons/era1_icon_5199C7_inverted.svg");
     }
 
-    /* Styles for .era-migration */
-    .story-navigation-button-and-label.era-migration:hover .story-navigation-button {
-        background-color: #FFA700;
+    .story-navigation-button-and-label.era-pre-colonial, .story-navigation-button-and-label.era-pre-colonial:hover .story-navigation-button, .story-navigation-button-and-label.era-pre-colonial.active .story-navigation-button, body.era-pre-colonial .story-narrative-footer {
+        background-color: #5199C7;
     }
-    .story-navigation-button-and-label.era-migration:hover .story-navigation-label{
-        color: #FFA700;
+
+    /* Styles for .era-early-european-settlement */
+    .story-navigation-button-and-label.era-early-european-settlement .story-navigation-button {
+        background-image: url("/images/icons/era2_icon_70AC00.svg");
+        border-color: #70AC00;
+    }
+    .story-navigation-button-and-label.era-early-european-settlement:hover .story-navigation-button {
+        background-image: url("/images/icons/era2_icon_70AC00_inverted.svg");
+    }
+
+    .story-navigation-button-and-label.era-early-european-settlement, .story-navigation-button-and-label.era-early-european-settlement:hover .story-navigation-button {
+        background-color: #70AC00;
+    }
+
+    /* Styles for .era-urban-industrial */
+    .story-navigation-button-and-label.era-urban-industrial .story-navigation-button {
+        background-image: url("/images/icons/era3_icon_9762AF.svg");
+        border-color: #9762AF;
+    }
+    .story-navigation-button-and-label.era-urban-industrial:hover .story-navigation-button {
+        background-image: url("/images/icons/era3_icon_9762AF_inverted.svg");
+    }
+
+    .story-navigation-button-and-label.era-urban-industrial, .story-navigation-button-and-label.era-urban-industrial:hover .story-navigation-button, .story-navigation-button-and-label.era-urban-industrial.active .story-navigation-button, body.era-urban-industrial .story-narrative-footer {
+        background-color: #9762AF;
+    }
+    
+    /* Styles for .era-migration */
+    .story-navigation-button-and-label.era-migration .story-navigation-button {
+        background-image: url("/images/icons/era4_icon_D0B000.svg");
+        border-color: #D0B000;
+    }
+    .story-navigation-button-and-label.era-migration:hover .story-navigation-button {
+        background-image: url("/images/icons/era4_icon_D0B000_inverted.svg");
+    }
+
+    .story-navigation-button-and-label.era-migration, .story-navigation-button-and-label.era-migration:hover .story-navigation-button {
+        background-color: #D0B000;
     }
 
     /* Styles for .era-activism-deindustrialization */
-    .story-navigation-button-and-label.era-activism-deindustrialization:hover .story-navigation-button {
-        background-color: #EF5402;
+    .story-navigation-button-and-label.era-activism-deindustrialization .story-navigation-button {
+        background-image: url("/images/icons/era5_icon_E36900.svg");
+        border-color: #E36900;
     }
-    .story-navigation-button-and-label.era-activism-deindustrialization:hover .story-navigation-label{
-        color: #EF5402;
+    .story-navigation-button-and-label.era-activism-deindustrialization:hover .story-navigation-button {
+        background-image: url("/images/icons/era5_icon_E36900_inverted.svg");
+    }
+
+    .story-navigation-button-and-label.era-activism-deindustrialization, .story-navigation-button-and-label.era-activism-deindustrialization:hover .story-navigation-button {
+        background-color: #E36900;
     }
 
     .story-navigation-button img {
@@ -164,45 +182,42 @@
         font-weight: bold;
     }
 
-    .story-navigation-button-group-header {
-        display: none;
+    .story-navigation-button-and-label {
+        /* display: block; */
+        background-color: white;
+        border-radius: 22.5px;
+        width: fit-content;
+        /* height: 35px; */
     }
 
     .story-navigation-button {
-            border: #8D8E8E 1px solid;
-            background-color: white;
-            border-radius: 50%;
-            width: 35px;
-            height: 35px;
-            line-height: 35px;
-            cursor: pointer;
-            text-align: center;
-            color: #8D8E8E;
-            font-size: 10px;
-            font-weight: bold;
-            margin: 3px;
+        background-color: white;
+        border-radius: 50%;
+        width: 35px;
+        height: 35px;
+        line-height: 35px;
+        cursor: pointer;
+        text-align: center;
+        color: #8D8E8E;
+        font-size: 10px;
+        font-weight: bold;
+        /* margin: 3px; */
     }
 
     .story-navigation-label{
         height: 35px;
         line-height: 35px;
-        color: #8D8E8E;
+        color: white;
         font-size: 10px;
         font-weight: bold;
-        margin: 3px;
+        /* margin: 3px; */
     }
 
     .story-navigation-label{
-        border: #8D8E8E 1px solid;
-        background-color: white;
         border-radius: 17.5px;
         height: 35px;
         width: 35px;
         padding: 0 10px;
-    }
-
-    .story-navigation-label span {
-        display: none;
     }
 
     /* may be able to animate this, but it's a bit hacky, e.g. this: https://stackoverflow.com/questions/3508605/how-can-i-transition-height-0-to-height-auto-using-css/8331169#8331169 */
@@ -210,19 +225,37 @@
         width: auto;
     }
 
-
-    .story-navigation-label:hover span {
-        display: inline;
-    }
-
     /* Mobile first */
 
-    .story-navigation-button {
+    /* .story-navigation-button {
         margin-right: 3px;
+    } */
+
+    .story-navigation-button-and-label {
+        margin: 0 3px;
+    }
+
+    .story-navigation-button {
+        padding-right: 3px;
     }
 
     .story-navigation-button-group .story-navigation-button {
+        display: block;
+    }
+
+    .story-navigation-label{
         display: none;
+        border: none; 
+        width: auto;
+        margin-right: 15px;
+    }
+
+    .story-navigation-label span{
+        display: inline;
+    }
+
+    .story-navigation-button-and-label:hover .story-navigation-label{
+        display: block;
     }
     
     /* Breakpoints at 576px, 768px, 992px, and 1200px (Bootstrap). */
@@ -243,44 +276,8 @@
             border-bottom: none;
         }
 
-        .story-navigation-button-group {
-            display: block;
-            background-color: white;
-            border: #8D8E8E 1px solid;
-            border-radius: 22.5px;
-            width: max-content;
-            /* width: 43px; */
-            /* height: 35px; */
-        }
-
-        .story-navigation-button-group-header {
-            display: block;
-            text-align: left;
-            margin: 5px 10px;
-            line-height: 10px;
-        }
-
-        .story-navigation-button {
-            margin-right: 3px;
-        }
-
-        .story-navigation-button-group .story-navigation-button {
-            display: block;
-        }
-
-        .story-navigation-label{
-            display: none;
-            border: none; 
-            width: auto;
-            margin-right: 15px;
-        }
-    
-        .story-navigation-label span{
-            display: inline;
-        }
-
-        .story-navigation-button-group:hover .story-navigation-label{
-            display: block;
+        .story-navigation-button-and-label {
+            margin: 3px 0;
         }
         
     }
