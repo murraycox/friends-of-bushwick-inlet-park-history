b80533002937578ed624f38e6ac348f70c8b9170
Murray Cox <code@murraycox.com>
2025-07-29 20:43:49 -0400

    Make sure chapter exists for all pages?

--- src/routes/era/[era]/[chapter]/+page.server.js
+++ src/routes/era/[era]/[chapter]/+page.server.js
@@ -88,84 +88,82 @@ export async function load({ params }) {
 
   const chapterData = items[0];
 
-    // from https://www.contentful.com/blog/rendering-linked-assets-entries-in-contentful/
-    // Create a bespoke renderOptions object to target BLOCKS.EMBEDDED_ENTRY (linked block entries e.g. code blocks)
-    // INLINES.EMBEDDED_ENTRY (linked inline entries e.g. a reference to another blog post)
-    // and BLOCKS.EMBEDDED_ASSET (linked assets e.g. images)
+  // from https://www.contentful.com/blog/rendering-linked-assets-entries-in-contentful/
+  // Create a bespoke renderOptions object to target BLOCKS.EMBEDDED_ENTRY (linked block entries e.g. code blocks)
+  // INLINES.EMBEDDED_ENTRY (linked inline entries e.g. a reference to another blog post)
+  // and BLOCKS.EMBEDDED_ASSET (linked assets e.g. images)
 
-    function renderOptions(links) {
-        
-        // create an asset map
-        const assetMap = new Map();
-        // loop through the assets and add them to the map
-        for (const asset of links.assets.block) {
-          assetMap.set(asset.sys.id, asset);
-        }
+  function renderOptions(links) {
       
-        // create an entry map
-        const entryBlockMap = new Map();
-        // loop through the block linked entries and add them to the map
-        for (const entry of links.entries.block) {
-          entryBlockMap.set(entry.sys.id, entry);
-        }
-      
-        // const entryMap = new Map();
-        // //  // loop through the inline linked entries and add them to the map
-        // for (const entry of links.entries.inline) {
-        //   entryMap.set(entry.sys.id, entry);
-        // }
-      
-        return {
+      // create an asset map
+      const assetMap = new Map();
+      // loop through the assets and add them to the map
+      for (const asset of links.assets.block) {
+        assetMap.set(asset.sys.id, asset);
+      }
+    
+      // create an entry map
+      const entryBlockMap = new Map();
+      // loop through the block linked entries and add them to the map
+      for (const entry of links.entries.block) {
+        entryBlockMap.set(entry.sys.id, entry);
+      }
+    
+      // const entryMap = new Map();
+      // //  // loop through the inline linked entries and add them to the map
+      // for (const entry of links.entries.inline) {
+      //   entryMap.set(entry.sys.id, entry);
+      // }
+    
+      return {
+        // other options...
+    
+        renderNode: {
           // other options...
-      
-          renderNode: {
-            // other options...
-            //  [INLINES.EMBEDDED_ENTRY]: (node, children) => {
-            //   // find the entry in the entryMap by ID
-            //   const entry = entryMap.get(node.data.target.sys.id);
-      
-            //   // render the entries as needed
-            //   if (entry.__typename === "BlogPost") {
-            //     return <a href={`/blog/${entry.slug}`}>{entry.title}</a>;
-            //   }
-            // },
-            [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
-              // find the entry in the entryMap by ID
-              const entry = entryBlockMap.get(node.data.target.sys.id);
-              // render the entries as needed by looking at the __typename 
-              // referenced in the GraphQL query
-              if (entry.__typename === "SpecialImage") {
-                return (
-                  `<div class="side-by-side-image">
-                    ${entry.imagesCollection.items.map((item) =>{
-                      return(
-                        `<figure class="figure">
-                          <img src="${item.url}" class="figure-img" alt="${item.title}">
-                          <figcaption class="figure-caption">${item.description}</figcaption>
-                         </figure>
-                      `)
-                    }).join('')
-                    }
-                  </div>`
-                );
-              }
-            },
-            [BLOCKS.EMBEDDED_ASSET]: (node, next) => {
-              // find the asset in the assetMap by ID
-              const asset = assetMap.get(node.data.target.sys.id);
-      
-              // render the asset accordingly
+          //  [INLINES.EMBEDDED_ENTRY]: (node, children) => {
+          //   // find the entry in the entryMap by ID
+          //   const entry = entryMap.get(node.data.target.sys.id);
+    
+          //   // render the entries as needed
+          //   if (entry.__typename === "BlogPost") {
+          //     return <a href={`/blog/${entry.slug}`}>{entry.title}</a>;
+          //   }
+          // },
+          [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+            // find the entry in the entryMap by ID
+            const entry = entryBlockMap.get(node.data.target.sys.id);
+            // render the entries as needed by looking at the __typename 
+            // referenced in the GraphQL query
+            if (entry.__typename === "SpecialImage") {
               return (
-                `<figure class="figure">
-                  <img src="${asset.url}" class="figure-img" alt="${asset.title}">
-                  <figcaption class="figure-caption">${asset.description}</figcaption>
-                </figure>`
+                `<div class="side-by-side-image">
+                  ${entry.imagesCollection.items.map((item) =>{
+                    return(
+                      `<figure class="figure">
+                        <img src="${item.url}" class="figure-img" alt="${item.title}">
+                        <figcaption class="figure-caption">${item.description}</figcaption>
+                        </figure>
+                    `)
+                  }).join('')
+                  }
+                </div>`
               );
-            },
+            }          },
+          [BLOCKS.EMBEDDED_ASSET]: (node, next) => {
+            // find the asset in the assetMap by ID
+            const asset = assetMap.get(node.data.target.sys.id);
+    
+            // render the asset accordingly
+            return (
+              `<figure class="figure">
+                <img src="${asset.url}" class="figure-img" alt="${asset.title}">
+                <figcaption class="figure-caption">${asset.description}</figcaption>
+              </figure>`
+            );
           },
-        };
-    };
-
+        },
+      };
+  };
 
   // convert content to HTML
   if (chapterData.content)
@@ -179,7 +177,7 @@ export async function load({ params }) {
   const thisEraChapterArray = Object.values(thisEra.chapters);
   thisEraChapterArray.forEach((chapter, index) => {
       console.log(`chapter: ${JSON.stringify(chapter)}, index: ${index}`);
-      if (chapter.id == thisChapter.id) 
+      if (chapter && thisChapter && (chapter.id == thisChapter.id)) 
           thisChapterIndexInStory = index;
   });
   //console.log(`thisChapterIndexInStory: ${thisChapterIndexInStory}`);
